b4aff9bc75da495ca678c582db052e793170dd06
Jose Luis <pepirob>
2023-03-14 18:59:58 +0100

    send created equipment id to frontend

--- routes/equipment.routes.js
+++ routes/equipment.routes.js
@@ -36,7 +36,7 @@ router.post("/", isAuthenticated, async (req, res, next) => {
       return;
     }
 
-    await Equipment.create({
+    const response = await Equipment.create({
       owner: _id,
       name,
       pricePerDay,
@@ -45,7 +45,9 @@ router.post("/", isAuthenticated, async (req, res, next) => {
       img,
     });
 
-    res.status(201).json();
+    console.log(response);
+
+    res.status(201).json(response._id);
   } catch (error) {
     next(error);
   }
